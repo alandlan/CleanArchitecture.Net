aad0af2cf1947039b35ff79d3fe02b941bdd088a
Alan Vieira Martins <alanhorcrux@gmail.com>
2025-02-05 15:34:05 -0300

    Teste gitignore

--- CleanArch/Domain/Interfaces/IUserRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using CleanArch.Domain.Entities;
-
-namespace CleanArch.Domain.Interfaces
-{
-    public interface IUserRepository : IBaseRepository<User>
-    {
-        Task<User?> GetByEmailAsync(string email,CancellationToken cancellationToken);
-    }
-}
